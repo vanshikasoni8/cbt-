a90cfed36ef8b20044c23722699d892130a240a6
Vanshika soni <106619869+vanshikasoni8@users.noreply.github.com>
2024-03-13 13:29:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const PORT = 4444;
+const path=require("path")
+const hbs=require("hbs")
+const mongoose = require('mongoose')
+
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({extended:true}))
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use("/",require("./router/blog"))
+
+
+
+mongoose.connect('mongodb://localhost:27017/BlogPage')
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log("http://localhost:"+PORT);
+    })
+})
